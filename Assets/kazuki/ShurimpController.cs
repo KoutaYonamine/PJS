bb1c7ff160ea0fc556e7d3a43d6c9a33db4a46fb
kazuki24 <kazu2324ki@gmail.com>
2019-10-30 11:35:07 +0900

    2週間ほど前のやつ

---
+++ Assets/kazuki/ShurimpController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Constant;
+
+public class ShurimpController : MonoBehaviour
+{
+    Rigidbody2D rig;
+    Vector2 Pposition;
+
+    bool RotaFlg;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        Pposition = transform.position;
+        RotaFlg = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    void Protate()
+    {
+        transform.Rotate(0, 0, -1f);
+    }
+    void Getkeystate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(transform.up * 3, ForceMode2D.Impulse);
+            RotaFlg = false;
+        }
+    }
+
+}
